e39368484158ef1c0c0131de7ffd6da7e7f325c2
yosra2001 <ouesletyyosra@gmail.com>
2024-02-18 18:07:52 +0100

    crud v2

--- backendPidev/src/main/java/com/pidev/backend/Controller/ConversationController.java
+++ backendPidev/src/main/java/com/pidev/backend/Controller/ConversationController.java
@@ -3,6 +3,7 @@ package com.pidev.backend.Controller;
 import com.pidev.backend.Entity.Conversation;
 import com.pidev.backend.Entity.User;
 import com.pidev.backend.Service.ConversationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,14 +11,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/conversations")
 public class ConversationController {
+
+    @Autowired
     ConversationService conversationService;
-    @PostMapping
-    public Conversation addConversation (@RequestBody Conversation c) {
+
+    @PostMapping("/add-conversation")
+    public Conversation addConversation(@RequestBody Conversation c) {
         return conversationService.addConversation(c);
     }
 
-    @GetMapping
+    @GetMapping("/retrieve-conversations")
     public List<Conversation> getAllConversations() {
         return conversationService.getALLConversations();
     }
+
+    @PutMapping ("/modify-conversation")
+    public Conversation modifyConversation(@RequestBody Conversation c) {
+        return conversationService.modifyConversation(c);
+    }
+    @DeleteMapping("/delete-conversation/{conversation-id}")
+    public void deleteConversation(@PathVariable("conversation-id") Long conversationId) {
+        conversationService.deleteConversation(conversationId);
+    }
+
+
+
 }
--- backendPidev/src/main/java/com/pidev/backend/ServiceImpl/ConversationServiceImpl.java
+++ backendPidev/src/main/java/com/pidev/backend/ServiceImpl/ConversationServiceImpl.java
@@ -1,25 +1,41 @@
 package com.pidev.backend.ServiceImpl;
 
 import com.pidev.backend.Entity.Conversation;
+import com.pidev.backend.Entity.User;
 import com.pidev.backend.Repository.ConversationRepository;
 import com.pidev.backend.Service.ConversationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
+@AllArgsConstructor
 public class ConversationServiceImpl implements ConversationService {
 
     ConversationRepository conversationRepository;
-
-
     @Override
     public Conversation addConversation(Conversation conversation) {
-
-            return conversationRepository.save(conversation);
+        return conversationRepository.save(conversation);
     }
 
     @Override
     public List<Conversation> getALLConversations() {
         return conversationRepository.findAll();
+    }
+
+    @Override
+    public Conversation modifyConversation(Conversation conversation) {
+        return conversationRepository.save(conversation) ;
+    }
+
+    @Override
+    public void deleteConversation(Long conversationId) {
+
+        conversationRepository.deleteById(conversationId);
+        System.out.println("siprimééééé");
+
     }
 
 
