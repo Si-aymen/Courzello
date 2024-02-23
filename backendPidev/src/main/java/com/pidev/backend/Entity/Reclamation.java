1e2021c3e3a8185fb72bef585a3a10da512393d6
aymen <aymen.rahali@esprit.tn>
2024-02-23 16:56:13 +0100

    Merged Reclamation to main prog

--- backendPidev/src/main/java/com/pidev/backend/Entity/Reclamation.java
+++ backendPidev/src/main/java/com/pidev/backend/Entity/Reclamation.java
@@ -1,29 +1,39 @@
 package com.pidev.backend.Entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Document(collection = "Reclamations")
+@Builder
+@ToString
 public class Reclamation {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long reclamationID;
+    @MongoId
+    private String reclamationID;
     private String dateSubmitted;
-    private String submittedBy;
     private String subject;
     private String description;
+    private ReclamationType type;
 
-//String juste bech testit bel postman, sinon tatbadel 3ala 7asb el attached files type
-    private String attachments;
+
+    //String juste bech testit bel postman, sinon tatbadel 3ala 7asb el attached files type
+    @ToString.Exclude
+    @DBRef
+    @JoinColumn(name = "user_id")
+    private User user;
+
+//private String attachments;
+
+
+    public Reclamation() {
+
+    }
 
 }
