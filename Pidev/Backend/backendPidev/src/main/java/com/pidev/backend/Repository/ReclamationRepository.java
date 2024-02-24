e465a4c09f29058e29e682854fb5e781d841e05f
aymen <aymen.rahali@esprit.tn>
2024-02-24 15:56:39 +0100

    delete extra files

--- Pidev/Backend/backendPidev/src/main/java/com/pidev/backend/Repository/ReclamationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pidev.backend.Repository;
-
-import com.pidev.backend.Entity.Reclamation;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReclamationRepository extends MongoRepository<Reclamation, String> {}
--- Pidev/Backend/backendPidev/src/main/java/com/pidev/backend/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pidev.backend.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.pidev.backend.Entity.User;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
