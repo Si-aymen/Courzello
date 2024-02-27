0a7c8fae55db0e7a82ba124dfab5abf0dc625e04
reziguialaa <alaa.rezigui@esprit.tn>
2024-02-27 13:54:21 +0100

    front office template

--- frontOfficePiDev/src/app/app-routing.module.ts
+++ frontOfficePiDev/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CoursesComponent} from "./courses/courses.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path: "home", component:HomeComponent},
+  {path: "courses", component:CoursesComponent},
+
+
+
+
+
+  {path:"**", component:HomeComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
