2a3733acc50b62f432b507680be8f48f7bb9463c
reziguialaa <alaa.rezigui@esprit.tn>
2024-02-28 12:45:03 +0100

    teachers component update

--- frontOfficePiDev/src/app/teachers/teachers.component.ts
+++ frontOfficePiDev/src/app/teachers/teachers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-teachers',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./teachers.component.css']
 })
 export class TeachersComponent {
+
+  teacherArray: any[] = [];
+  id: String = "";
+  login: String = "";
+  password: string = "";
+  firstName: string ="";
+  lastName : string = "" ;
+  email : string ="";
+  dateOfBirth : Date =new Date();
+  speciality: string = "";
+
+
+
+  constructor(private http: HttpClient) {
+    this.GetAllTeachers();
+  }
+
+  ngOnInit(): void {}
+
+  GetAllTeachers() {
+    this.http.get("http://localhost:8090/pi/users/teachers").subscribe((resultData: any) => {
+      console.log(resultData);
+      this.teacherArray = resultData;
+
+
+
+    });
+  }
 
 }
