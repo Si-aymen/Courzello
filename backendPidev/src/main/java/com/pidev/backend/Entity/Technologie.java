c9e2c13ef566abf48ad335d82eb3204e7b9f16b4
omniamaktouf9 <omnia.maktouf15@gmail.com>
2024-03-07 10:29:10 +0100

    new push loading

--- backendPidev/src/main/java/com/pidev/backend/Entity/Technologie.java
+++
@@ -1,9 +0,0 @@
-package com.pidev.backend.Entity;
-
-public enum Technologie {
-    HTML, CSS, JavaScript, ReactJS, Angular, VueJS, Bootstrap,
-    Sass, jQuery, TypeScript, EmberJS, BackboneJS, WebAssembly, GraphQL, HandlebarsJS, Pug,
-    NodeJS, Django, Flask, RubyOnRails, ExpressJS, ASPNET, SpringBoot, Laravel, Symfony, FastAPI,
-    NestJS, Gin, Echo, KoaJS, MeteorJS, MySQL, PostgreSQL, MongoDB, SQLite, MicrosoftSQLServer, OracleDatabase,
-    Redis, FirebaseRealtimeDatabase, AmazonDynamoDB, Couchbase
-}
