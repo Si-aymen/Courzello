1350f0e506084cc904aba7507c1911849af38d82
aymen <aymen.rahali@esprit.tn>
2024-04-03 15:51:38 +0200

    add FrontOffice (user interface)

---
+++ Angular frontOffice/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CoursesComponent} from "./courses/courses.component";
+import {TeachersComponent} from "./teachers/teachers.component";
+import { CourseformComponent } from './courseform/courseform.component';
+import { ClassroomComponent } from './classroom/classroom.component';
+import { ClassroomDetailsComponent } from './classroom-details/classroom-details.component';
+
+const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path: "home", component:HomeComponent},
+  {path: "courses", component:CoursesComponent},
+  {path: "teachers", component:TeachersComponent},
+  {path: "addcourse", component:CourseformComponent},
+  {path: "classroom", component:ClassroomComponent},
+  {
+    path: 'Classroom/Details/:id', 
+    component: ClassroomDetailsComponent, 
+  },
+  
+
+
+
+
+
+  {path:"**", component:HomeComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
