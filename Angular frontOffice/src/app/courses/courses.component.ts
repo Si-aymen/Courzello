7a390ae80828b7ffa17ceb74ed873989a5982820
aymen <aymen.rahali@esprit.tn>
2024-04-17 17:53:26 +0200

    add rating to front

--- Angular frontOffice/src/app/courses/courses.component.ts
+++ Angular frontOffice/src/app/courses/courses.component.ts
@@ -61,5 +61,12 @@ export class CoursesComponent {
     });
   }
 
+  getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+
+
 
 }
